adf48fd9ba08100f1e0163ec61583ccf9fece5f8
Dixie Lang <dixie.lang@ymail.com>
2016-01-28 17:52:49 -0600

    To comment out the sidebar include to increase page width.

--- twentyfourteen_for_pddc/Fact_Sheet_Listing_on_Posts.php
+++ twentyfourteen_for_pddc/Fact_Sheet_Listing_on_Posts.php
@@ -170,9 +170,11 @@ wp_reset_query();
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
-	<?php get_sidebar( 'content' ); ?>
+	<?php 
+	/* get_sidebar( 'content' ); */
+	?>
 </div><!-- #main-content -->
 
 <?php
-get_sidebar();
+/* get_sidebar(); */
 get_footer();
