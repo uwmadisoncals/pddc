5138c86b0e42e8a1e3797d298c0d51d2c7189211
Dixie Lang <dixie.lang@ymail.com>
2016-01-28 17:35:28 -0600

    Added includes folder with Acrobat notice.

---
+++ twentyfourteen_for_pddc/includes/AcrobatNotice.php
@@ -0,0 +1,19 @@
+<div style="margin-left: auto; margin-right: auto; border: 1px dotted #2b552b; padding: 10px;">
+<strong>For best viewing on mobile devices, turn your device landscape.</strong><br/><br/>
+<strong>Attention Firefox users:</strong><br/>
+
+<p style="margin-bottom: .5em;">
+If you would like to print the PDF version of the fact sheets on this page, instead of clicking the PDF links below to open them on the webserver, save the fact sheets to your computer, then use Adobe Reader to open them.<br />
+</p>
+
+<p style="margin-bottom: .5em;">
+If you would like a copy of a fact sheet in Word format, please email <a href="mailto:hudelson@wisc.edu">Brian Hudelson</a>.
+</p>
+
+<p style="margin-bottom: .5em;">
+To save the fact sheets to your computer, right-click the PDF link and choose Save Link As or Save Target As from the popup menu.<br/>
+</p>
+You can download Adobe Reader from Adobe's site - <a href="http://get.adobe.com/reader/">http://get.adobe.com/reader/</a> - make sure you uncheck "Yes, install McAfee Security Scan Plus - optional" before you click the Download now button.
+
+</div>
+
