292030783978f93afe89ec755c003768838ab9be
Dixie Lang <dixie.lang@ymail.com>
2016-01-31 18:02:50 -0600

    Formatting code.

--- twentyfourteen_for_pddc/single-fact-sheet.php
+++ twentyfourteen_for_pddc/single-fact-sheet.php
@@ -47,20 +47,15 @@ width:0px !important;
 
 
 <div style="background-color: #99CCFF; padding-top: .75em; padding-bottom: .5em; padding-left: .5em; padding-right: .5em;" >
-
 	<div style="font-size: 1.5em; font-style: italic; color: black; text-align: center;">University of Wisconsin Garden Fact Sheets</div>
-
 </div>
 
 <div style="text-align: center;">
-<img src="<?php echo $blog_path ?>/wp-content/themes/custom_footers/images/uwex-logo.png" alt="UWEX logo" style="padding-top: 1.25em; padding-bottom: .5em;">
+	<img src="<?php echo $blog_path ?>/wp-content/themes/custom_footers/images/uwex-logo.png" alt="UWEX logo" style="padding-top: 1.25em; padding-bottom: .5em;">
 </div>
 
 <header class="entry-header">
-
-<h1 style="color: #28361B;" class="entry-title"><?php the_title(); ?></h1>
-
-
+	<h1 style="color: #28361B;" class="entry-title"><?php the_title(); ?></h1>
 </header>
 
 
@@ -77,86 +72,96 @@ while ( have_posts() ) : the_post(); ?>
 	}
 ?>		
 
-<div> <span class="field-labels">Authors:</span> <?php printf( get_field('authors')); ?></div>
+<div> 
+	<span class="field-labels">Authors:</span> <?php printf( get_field('authors')); ?>
+</div>
 
-<div> <span class="field-labels">Last Revised:</span> <?php 
-$date = DateTime::createFromFormat('Ymd', get_field('last_revised'));
-echo $date->format('m/d/Y');
-?></div>
+<div> 
+	<span class="field-labels">Last Revised:</span> 
+	<?php 
+	$date = DateTime::createFromFormat('Ymd', get_field('last_revised'));
+	echo $date->format('m/d/Y');
+	?>
+</div>
 
-<div><span class="field-labels">X-number:</span> <?php printf( get_field('fact_sheet_x-number')); ?></div>
+<div>
+	<span class="field-labels">X-number:</span> <?php printf( get_field('fact_sheet_x-number')); ?>
+</div>
 
 <div style="padding-top: 1.3em; text-align: justify;">	
-	<?php 
-	
-	the_content();
+	<?php 	
+		the_content();
 	?>
-
 </div>
 
 <hr />
 
-<div>This Fact Sheet is also available in PDF format:</div>
+<div>
+	This Fact Sheet is also available in PDF format:
+</div>
 
 <ul>
-<li><a href="<?php echo $blog_path ?>/files/Fact_Sheets/FC_PDF/<?php printf( get_field('fact_sheet_filename_stem')) ?>.pdf" target="_blank">Full Color PDF</a></li>
-<li><a href="<?php echo $blog_path ?>/files/Fact_Sheets/LC_PDF/<?php printf( get_field('fact_sheet_filename_stem')) ?>.pdf" target="_blank">Low Color PDF</a></li>
+	<li>
+		<a href="<?php echo $blog_path ?>/files/Fact_Sheets/FC_PDF/<?php printf( get_field('fact_sheet_filename_stem')) ?>.pdf" target="_blank">Full Color PDF</a>
+	</li>
+	<li>
+		<a href="<?php echo $blog_path ?>/files/Fact_Sheets/LC_PDF/<?php printf( get_field('fact_sheet_filename_stem')) ?>.pdf" target="_blank">Low Color PDF</a>
+	</li>
 </ul>
 
 <hr />
 
-
 <div style="font-size: 80%;">
 
+	<?php
+	/* conditionally display the completed as text if the boolean value is true */ 	
+		if ((get_field('completed_as_partial_fulfillment_for_class'))) {
+			echo '<p>';	
+			printf( get_field('completed_as_partial_fulfillment_for_class'));	
+			echo '</p>';	
+			}
+	?>		
 
+	<?php
 
-<?php
-/* conditionally display the completed as text if the boolean value is true */ 	
-	if ((get_field('completed_as_partial_fulfillment_for_class'))) {
-		echo '<p>';	
-		printf( get_field('completed_as_partial_fulfillment_for_class'));	
-		echo '</p>';	
-		}
-?>		
-<?php
-
-/* conditionally display the completed as text if the boolean value is true */ 	
-	if ((get_field('stanosz_copyright_statement'))) {
-		echo '<p>';	
-		echo '&copy; ';
-		printf( get_field('copyright_years_covered') );			
-		echo ' Glen Stanosz All Rights Reserved.';	
-		echo '</p>';	
-		}
-		else {
-		echo '<p>';	
-		echo '&copy; ';
-		printf( get_field('copyright_years_covered') );	
-		echo ' the Board of Regents of the University of Wisconsin System doing business as the division of Cooperative Extension of the University of Wisconsin Extension.';	
-		echo '</p>';	
-		}
-?>	
+	/* conditionally display the completed as text if the boolean value is true */ 	
+		if ((get_field('stanosz_copyright_statement'))) {
+			echo '<p>';	
+			echo '&copy; ';
+			printf( get_field('copyright_years_covered') );			
+			echo ' Glen Stanosz All Rights Reserved.';	
+			echo '</p>';	
+			}
+			else {
+			echo '<p>';	
+			echo '&copy; ';
+			printf( get_field('copyright_years_covered') );	
+			echo ' the Board of Regents of the University of Wisconsin System doing business as the division of Cooperative Extension of the University of Wisconsin Extension.';	
+			echo '</p>';	
+			}
+	?>	
 	
 
 <p>An EEO/Affirmative Action employer, University of Wisconsin Extension provides equal opportunities in employment and programming, including Title IX and ADA requirements.  This document can be provided in an alternative format by calling Brian Hudelson at (608) 262-2863 (711 for Wisconsin Relay). </p> 
 
 <?php
-/* conditionally display the pesticide text if the boolean value is true */ 	
-	if ((get_field('pesticide_statement'))) {		
-		echo '<p>References to pesticide products in this publication are for your convenience and are not an endorsement or criticism of one product over similar products.  You are responsible for using pesticides according to the manufacturer’s current label directions.  Follow directions exactly to protect the environment and people from pesticide exposure.  Failure to do so violates the law.</p>';	
-	}
+	/* conditionally display the pesticide text if the boolean value is true */ 	
+		if ((get_field('pesticide_statement'))) {		
+			echo '<p>References to pesticide products in this publication are for your convenience and are not an endorsement or criticism of one product over similar products.  You are responsible for using pesticides according to the manufacturer’s current label directions.  Follow directions exactly to protect the environment and people from pesticide exposure.  Failure to do so violates the law.</p>';	
+		}
 ?>		
 
 <?php
-/* conditionally display the thanks text if the boolean value is true */ 	
-	if ((get_field('thanks_to'))) {
-		echo '<p>';	
-		printf( get_field('thanks_to'));	
-		echo '</p>';	
-		}
+	/* conditionally display the thanks text if the boolean value is true */ 	
+		if ((get_field('thanks_to'))) {
+			echo '<p>';	
+			printf( get_field('thanks_to'));	
+			echo '</p>';	
+			}
 ?>	
 
 <p>A complete inventory of University of Wisconsin Garden Facts is available at the University of Wisconsin-Extension Plant Disease Diagnostics Clinic website:  <a href="http://pddc.wisc.edu">http://pddc.wisc.edu</a>.</p>
+
 </div>
 
 </div> 
@@ -165,12 +170,12 @@ echo $date->format('m/d/Y');
 
 <?php 
 
-if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
- the_post_thumbnail(array(400,400));
-} 
-?>
+	if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+	 the_post_thumbnail(array(400,400));
+	} 
+	?>
 
-</div>
+</div><!-- end post thumbnail div -->
 
 <?php
 
@@ -181,8 +186,8 @@ wp_reset_query();
 
 ?>
 
-</div><!-- #content -->
-</div><!-- #primary -->
+</div><!-- end #content -->
+</div><!-- end #primary -->
 
 <?php 
 get_sidebar();
