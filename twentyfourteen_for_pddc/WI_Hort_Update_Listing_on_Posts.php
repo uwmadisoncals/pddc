f6de22cccf2f74bbea01b052fb6531cbd1d7242d
Dixie Lang <dixie.lang@ymail.com>
2016-03-15 15:12:10 -0500

    Template tweak.

--- twentyfourteen_for_pddc/WI_Hort_Update_Listing_on_Posts.php
+++ twentyfourteen_for_pddc/WI_Hort_Update_Listing_on_Posts.php
@@ -35,9 +35,30 @@ get_header();
 <?php
 
 /* Count the number of records returned by the query for display in the search results header */
-$num_of_records_returned = $wp_query->post_count; 
-echo '<p>There are '.$num_of_records_returned.' Fact Sheet(s): </p>';
+//$num_of_records_returned = $wp_query->post_count; 
+//echo '<p>There are '.$num_of_records_returned.' Wisconsin Hort Update Archive Record(s): </p>';
 	
+	if (have_posts()) :	
+		
+	/* Begin the unordered list */	
+	echo '<ol>'; // begin the unordered list	
+
+		while (have_posts()) : the_post(); ?>
+		
+		<?php echo '<li>';	?>
+		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+		
+		<?php echo '</li>'; 
+		
+			endwhile;
+
+		endif; 		
+		
+		echo '</ol>';
+		
+		?>
+		
+
 	
 ?>	
 
