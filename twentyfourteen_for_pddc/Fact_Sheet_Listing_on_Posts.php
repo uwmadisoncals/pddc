02b619b0423d5536c6ea794ea0e271ce8a78ed32
Dixie Lang <dixie.lang@ymail.com>
2016-04-07 15:55:20 -0500

    Change function to site_url()

--- twentyfourteen_for_pddc/Fact_Sheet_Listing_on_Posts.php
+++ twentyfourteen_for_pddc/Fact_Sheet_Listing_on_Posts.php
@@ -111,7 +111,7 @@ while (have_posts()) : the_post();
 
 		echo "<td style='text-align: center; border: 1px solid #28361b;'>";
 		
-		?><br /><?php echo 'blog path is - '.$blog_path; ?><br />
+		?><br /><?php echo 'blog path is - '.$site_url(); ?><br />
 		<a href="<?php echo $blog_path ?>/files/Fact_Sheets/FC_PDF/<?php printf( get_field('fact_sheet_filename_stem')) ?>.pdf" target="_blank">Full Color PDF</a><br />
 		
 		<a href="http://labs.russell.wisc.edu/pddc/files/Fact_Sheets/FC_PDF/<?php printf( get_field('fact_sheet_filename_stem')) ?>.pdf" target="_blank">PDF</a><?php
