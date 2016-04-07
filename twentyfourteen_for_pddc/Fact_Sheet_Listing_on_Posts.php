33244efc7df597291672376f66b981c7c4cd70de
Dixie Lang <dixie.lang@ymail.com>
2016-04-07 16:01:53 -0500

    still tweaking URL function.

--- twentyfourteen_for_pddc/Fact_Sheet_Listing_on_Posts.php
+++ twentyfourteen_for_pddc/Fact_Sheet_Listing_on_Posts.php
@@ -106,13 +106,14 @@ while (have_posts()) : the_post();
 		echo "</td>";			
 		
 
-		$url = network_site_url();
-		echo $url;
+		global $current_blog; 
+		$blog_path = $current_blog->path; 
+		
 		// Full Color PDF 
 
 		echo "<td style='text-align: center; border: 1px solid #28361b;'>";
 		
-		?><br /><?php echo 'blog path is - '.$url; ?><br />
+		?><br /><?php echo 'blog path is - '.$blog_path; ?><br />
 		<a href="<?php echo $blog_path ?>/files/Fact_Sheets/FC_PDF/<?php printf( get_field('fact_sheet_filename_stem')) ?>.pdf" target="_blank">Full Color PDF</a><br />
 		
 		<a href="http://labs.russell.wisc.edu/pddc/files/Fact_Sheets/FC_PDF/<?php printf( get_field('fact_sheet_filename_stem')) ?>.pdf" target="_blank">PDF</a><?php
