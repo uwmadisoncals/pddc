d953784fbbe7bfa359f1f2b9e87409a5023375c2
Dixie Lang <dixie.lang@ymail.com>
2016-05-09 17:02:39 -0500

    Correct typo - low color Word link was going to FC Word directory

--- twentyfourteen_for_pddc/includes/FactSheetDisplayCode.php
+++ twentyfourteen_for_pddc/includes/FactSheetDisplayCode.php
@@ -92,7 +92,7 @@ while (have_posts()) : the_post();
 		
 		echo "<td style='text-align: center; border: 1px solid #28361b;'>"; ?>
 		
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/blogs.dir/39/files/Fact_Sheets/FC_Word/<?php printf( get_field('fact_sheet_filename_stem')) ?>.doc" target="_blank">Word</a><?php
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/blogs.dir/39/files/Fact_Sheets/LC_Word/<?php printf( get_field('fact_sheet_filename_stem')) ?>.doc" target="_blank">Word</a><?php
 		
 		echo "</td>";
 		
