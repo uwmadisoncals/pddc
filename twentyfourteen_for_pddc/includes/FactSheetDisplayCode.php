7b567786f6d68d4360b7554351dcf4be76ebc05b
Dixie Lang <dixie.lang@ymail.com>
2016-06-09 11:28:38 -0500

    Reverse changes.

--- twentyfourteen_for_pddc/includes/FactSheetDisplayCode.php
+++ twentyfourteen_for_pddc/includes/FactSheetDisplayCode.php
@@ -53,14 +53,7 @@ while (have_posts()) : the_post();
 		$thispost = $post->ID;
 		$fact_sheet_direct_edit_string = $blog_path."/wp-admin/post.php?post=".$thispost."&action=edit";
 
-			/* display edit fact sheet link only if they're logged in and have security priv to work with the media library and upload files */
-			if (current_user_can('upload_files')) {
-				?><a href="<?php echo $fact_sheet_direct_edit_string ?>" target="_blank">Edit Fact Sheet</a>&nbsp;&nbsp; - <? 
-				;
-				}  		
-		
 		the_title();
-		echo "test";
 
 		echo "</td>";
 		
