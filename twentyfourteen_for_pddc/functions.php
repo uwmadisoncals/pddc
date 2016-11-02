fdb814d53d9ff3d05ef6e1994844f5007e601125
Dixie Lang <dixie.lang@ymail.com>
2016-11-02 16:34:25 -0500

    Remove code that registered the widget area.

--- twentyfourteen_for_pddc/functions.php
+++ twentyfourteen_for_pddc/functions.php
@@ -33,24 +33,6 @@ add_filter( 'attachment_fields_to_save', 'pddc_archived_handouts_save', 10, 2 );
 
 /***********************************************************************/
 
-/* This adds a new widget area below the menu bar and above the page title to display a ticker for informational messages */	
-
-/**
- * Register Widget Area.
- *
- */
-function info_bulletin_area_widgets_init() {
-
-	register_sidebar( array(
-		'name' => 'Info Bulletin Area',
-		'id' => 'info_bulletin_area',
-		'before_widget' => '<div id="info-bulletin-area-widgets">',
-		'after_widget' => '</div>',
-		'before_title' => '<h2 class="rounded">',
-		'after_title' => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'info_bulletin_area_widgets_init' );
 
 
 ?>
