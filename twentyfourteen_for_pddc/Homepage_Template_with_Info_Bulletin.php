d879c8b68c6236a1f4cc819b89ac10d6aab66deb
Dixie Lang <dixie.lang@ymail.com>
2016-11-02 13:22:59 -0500

    Change the query to get post id 6.

--- twentyfourteen_for_pddc/Homepage_Template_with_Info_Bulletin.php
+++ twentyfourteen_for_pddc/Homepage_Template_with_Info_Bulletin.php
@@ -45,7 +45,9 @@ endif; ?>
 					 * use this in a child theme, then include a file called called content-___.php
 					 * (where ___ is the post format) and that will be used instead.
 					 */
-					get_template_part( 'content', get_post_format() );
+					$args = array ('ID' => 6; 
+					 
+					get_template_part( 'content', get_post_format($args) );
 
 					// Previous/next post navigation.
 					twentyfourteen_post_nav();
