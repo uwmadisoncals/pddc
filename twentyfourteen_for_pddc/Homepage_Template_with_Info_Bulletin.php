3cfc84a3dd320028a6a7146f7e465849f763de63
Dixie Lang <dixie.lang@ymail.com>
2016-11-02 15:30:01 -0500

    Filter page to page 6 - second try.

--- twentyfourteen_for_pddc/Homepage_Template_with_Info_Bulletin.php
+++ twentyfourteen_for_pddc/Homepage_Template_with_Info_Bulletin.php
@@ -38,28 +38,10 @@ endif; ?>
 		
 			<?php
 				$post_id = 6;
-				
-				
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					
-					 
-					get_template_part( 'content', get_post_format($post_id) );
-
-					// Previous/next post navigation.
-					twentyfourteen_post_nav();
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
+				$queried_post = get_post($post_id);
+				$title = $queried_post->post_title;
+				echo $title;
+				echo $queried_post->post_content;
 			?>
 		
 		<!-- insert loop here - end -->
